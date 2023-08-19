9c24e131c3b475aee8c0575ccaa730e1945322ce
Jyunri <nekojimy@gmail.com>
2023-08-19 20:17:41 -0300

    feat: add content embedd calculator

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_19_202640) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_19_211319) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contents", force: :cascade do |t|
+    t.string "title"
+    t.string "category"
+    t.string "kind"
+    t.text "raw_body"
+    t.text "body"
+    t.json "embedding"
+    t.string "updated_at", limit: 50
+  end
 
   create_table "tags", force: :cascade do |t|
     t.string "name", null: false
