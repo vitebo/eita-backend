72c5f4a06570de4dc0497db59c52cf7561d1f2a0
vitebo <andre.vitebo@quero.com>
2023-08-19 17:25:15 -0300

    implementes mock create user

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,15 @@
 class UserController < ApplicationController
   def create
+    interests = params[:interests]
+
+    # check if interests is an array with strings
+    if interests.is_a?(Array) && interests.all? { |i| i.is_a?(String) } do
+      # respond erros to front with bad request
+      render json: { error: 'Interests must be an array of strings' }, status: :bad_request
+    end
+
+    render json: {
+      id: '1234567890',
+    }.to_json
   end
 end
