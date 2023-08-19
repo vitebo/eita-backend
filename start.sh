66c7f61f5c02c9f97af6e6a087c78f0dff6ca66d
vitebo <andre.vitebo@quero.com>
2023-08-19 18:18:48 -0300

    add bundle install when start

--- start.sh
+++ start.sh
@@ -3,12 +3,13 @@ set -e
 
 # export RAILS_ENV=production
 
+# install gems
+bundle check || bundle install
+
 # setup-db
 bundle exec rails db:create
 bundle exec rails db:migrate
 
-rm -rf /app/tmp/pids/server.pid
-
 # start project
-rm /app/tmp/pids/server.pid
+rm -rf /app/tmp/pids/server.pid
 bundle exec rails s -b 0.0.0.0 -p 3001
