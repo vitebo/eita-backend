191fae38c4971e86fa0cf055a60216d05d5544ad
Gabriela Midori Restani <gabriela.restani@redealumni.com>
2023-08-20 08:58:54 -0300

    fix: image_loader script

--- app/scripts/images_loader.rb
+++ app/scripts/images_loader.rb
@@ -3,7 +3,7 @@ require 'csv'
 class ImagesLoader
   def initialize
     CSV.foreach("tmp/revista-images.csv", headers: true) do |row|
-      Content.find(row['id']).update_columns(image_url: row['slug'])
+      Content.find(row['id']).update_columns(image_url: row['cover_image_url'])
     rescue ActiveRecord::RecordNotFound => e
       next
     end
