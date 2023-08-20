5e307506267e38b1ec0199db6c17a4d3b151e515
Jyunri <nekojimy@gmail.com>
2023-08-19 22:37:56 -0300

    feat: init embedding like and dislike

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,11 @@ class User < ApplicationRecord
 
   def calculate_embedding
     tags = self.tags.map(&:name)
-    EmbeddingCalculator.new.calculate_embedding(tags)
+    liked_contents = UserContentClassification.where(user_id: self.id, classification: 'like').map(&:content_id)
+    disliked_contents = UserContentClassification.where(user_id: self.id, classification: 'dislike').map(&:content_id)
+
+    embedded_tags = EmbeddingCalculator.new.calculate_embedding(tags)
+    embedded_liked_contents = liked_contents.map { |content_id| Content.find(content_id).embedding }
+    embedded_disliked_contents = disliked_contents.map { |content_id| Content.find(content_id).embedding }
   end
 end
